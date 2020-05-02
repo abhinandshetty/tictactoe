1a4771c323e35263e231c523eec249f26730911c
abhinandshetty <abhinandshetty.13@gmail.com>
2020-05-02 12:52:07 +0530

    Added designs for mode and piece selection components

--- src/components/ModeSelection.jsx
+++ src/components/ModeSelection.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {
+    Link
+} from 'react-router-dom';
 
 function ModeSelection() {
     return (
         <div className="container mx-auto mt-5">
             <div className="m-auto" style={{ width: 300, height: '400px' }}>
-                <div className="row" style={{marginLeft:40}}>
+                <div className="row" style={{ marginLeft: 45 }}>
                     <div className="block-x ml-3" style={{ fontSize: 130 }}>X</div>
                     <div className="block-o mr-3" style={{ fontSize: 130 }}>O</div>
                 </div>
@@ -13,10 +16,14 @@ function ModeSelection() {
                     <h6 className="m-auto">Choose your play mode</h6>
                 </div>
                 <div className="row mt-5">
-                    <button className="btn btn-primary m-auto custom-btn" style={{width: 150}}>With AI</button>
+                    <Link to="/piece-selection" className="btn btn-primary m-auto custom-btn" style={{ width: 150 }}>
+                        With AI
+                    </Link>
                 </div>
                 <div className="row mt-3">
-                    <button className="btn bg-white m-auto custom-btn" style={{width: 150}}>With a friend</button>
+                    <Link to="/piece-selection" className="btn bg-white m-auto custom-btn" style={{ width: 150 }}>
+                        With a friend
+                    </Link>
                 </div>
             </div>
         </div>
--- src/components/PieceSelection.jsx
+++ src/components/PieceSelection.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PieceSelection() {
     return (
-        <div>
-            pieceselection
+        <div className="container mx-auto mt-5">
+            <div className="m-auto" style={{ width: 300, height: '400px' }}>
+                <div className="row">
+                    <h6 className="m-auto">Pick your side</h6>
+                </div>
+                <div className="row mx-auto mt-3">
+                    <div className="col">
+                        <div className="block-x" style={{ fontSize: 130 }}>X</div>
+                    </div>
+                    <div className="col">
+                        <div className="block-o mr-3" style={{ fontSize: 130 }}>O</div>
+                    </div>
+                </div>
+                <div className="row mx-auto">
+                    <div className="col text-center">
+                        <input type="radio" name="preference" id="x" style={{ width: 25, height: 25 }} checked />
+                    </div>
+                    <div className="col text-center">
+                        <input type="radio" name="preference" id="o" style={{ width: 25, height: 25 }} />
+                    </div>
+                </div>
+
+                <div className="row mt-5">
+                    <Link to="/gameplay" className="btn bg-white m-auto custom-btn" style={{ width: 150 }}>
+                        Continue
+                    </Link>
+                </div>
+
+            </div>
         </div>
     )
 }
